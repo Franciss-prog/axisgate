71b0c65612a2cfc51516d8a8c248b5732bb0f3a1
francissprog <francissprog@gmail.com>
2025-11-20 21:14:39 +0800

    hell yeah

--- utils/conversion.ts
+++ utils/conversion.ts
@@ -1,19 +1,17 @@
 import type { Coordinates } from '@/app/page';
-// cob
 export const convertNetherToOverworld = (netherCoords: Coordinates) => {
-
-  return {
-    x: Math.floor(Number(netherCoords.x) * 8),
-    y: Number(netherCoords.y),
-    z: Math.floor(Number(netherCoords.z) * 8),
+    return {
+      x: Number(netherCoords.x) * 8,
+      y: Number(netherCoords.y),
+      z: Number(netherCoords.z) * 8,
+    };
   };
-};
-
-// convert the overworld coordinates to nether coordinates
-export const convertOverworldToNether = (overworldCoords: Coordinates) => {
-  return {
-    x: Math.floor(Number(overworldCoords.x) / 8),
-    y: Number(overworldCoords.y),
-    z: Math.floor(Number(overworldCoords.z) / 8),
+  
+  export const convertOverworldToNether = (overworldCoords: Coordinates) => {
+    return {
+      x: Number(overworldCoords.x) / 8,
+      y: Number(overworldCoords.y),
+      z: Number(overworldCoords.z) / 8,
+    };
   };
-};
+  
